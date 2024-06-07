94f6447f58a427d78db1ecf5edb76af59672f457
Mehedi Hasan <mehediyyzz@gmail.com>
2024-06-07 17:26:32 +0600

    setup shadcn

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,90 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+    "./app/**/*.{js,jsx}",
+    "./src/**/*.{js,jsx}",
+  ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       colors: {
-        brand: {
-          primary: "#14A077",
-          dark: "#123841",
+        primary: {
+          500: "#624CF5",
+          50: " #F6F8FD",
+          DEFAULT: "#624CF5",
+          foreground: "hsl(var(--primary-foreground))",
         },
-        dark: "#F5F5F2",
-        darker: "#E9EBEB",
-        error: "#ED455D",
+        coral: {
+          500: "#15BF59",
+        },
+
+        grey: {
+          600: "#545454",
+          500: "#757575",
+          400: "#AFAFAF",
+          50: "#F6F6F6",
+        },
+        black: "#000000",
+        white: "#FFFFFF",
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        foreground: "hsl(var(--foreground))",
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
       },
-      textColor: {
-        light: "#64666C",
-        primary: "#121212",
-        invert: "#FFFFFF",
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
-      backgroundColor: {
-        base: "#FFFFFF",
-        primary: "#FFFFFF",
-        dark: "#F5F5F2",
-        darker: "#E9EBEB",
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
       },
-      outlineColor: {
-        brand: "#14A077",
-        primary: "#E9EBEB",
-        error: "#ED455D",
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
