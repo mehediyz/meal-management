4a3fcd4ccfd77f4c00a53c98c6baeceb04d5447f
Mehedi Hasan <mehediyyzz@gmail.com>
2024-06-07 11:35:53 +0600

    colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,33 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          primary: "#14A077",
+          dark: "#123841",
+        },
+        dark: "#F5F5F2",
+        darker: "#E9EBEB",
+        error: "#ED455D",
+      },
+      textColor: {
+        light: "#64666C",
+        primary: "#121212",
+        invert: "#FFFFFF",
+      },
+      backgroundColor: {
+        base: "#FFFFFF",
+        primary: "#FFFFFF",
+        dark: "#F5F5F2",
+        darker: "#E9EBEB",
+      },
+      outlineColor: {
+        brand: "#14A077",
+        primary: "#E9EBEB",
+        error: "#ED455D",
+      },
+    },
   },
   plugins: [],
 };
