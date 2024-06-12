2ba95b1b51b4518c7ba53904999256d0a094a3ae
Mehedi Hasan <mehediyyzz@gmail.com>
2024-06-13 00:09:26 +0600

    show first name on chart

--- src/components/Charts/DepositExpenseBarChart.jsx
+++ src/components/Charts/DepositExpenseBarChart.jsx
@@ -4,7 +4,7 @@ import { deposits } from "@/constants/data";
 
 const DepositsData = deposits.map((item) => item.deposit);
 const ExpensesData = [300, 900, 1500, 2000, 1700, 1400, 2500, 1800, 3200, 1000];
-const xLabels = deposits.map((item) => item.name);
+const xLabels = deposits.map((item) => item.name.split(" ")[0]);
 
 export default function DepositExpenseBarChart() {
   return (
