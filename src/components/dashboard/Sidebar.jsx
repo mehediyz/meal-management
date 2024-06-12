015bb1f24f5bf8e6af26b1ecf95450c44881b91d
Mehedi Hasan <mehediyyzz@gmail.com>
2024-06-12 06:11:16 +0600

    dashboard sidebar link updated

--- src/components/dashboard/Sidebar.jsx
+++ src/components/dashboard/Sidebar.jsx
@@ -1,10 +1,27 @@
 import { NavLink } from "react-router-dom";
 import { dashboardNavLinks } from "../../constants";
+import { LayoutDashboard } from "lucide-react";
 
 const Sidebar = () => {
   return (
     <div className="min-h-screen w-[250px]">
       <div className="flex flex-col">
+        <NavLink
+          to={"/dashboard"}
+          className={({ isActive }) =>
+            `px-6 py-3 ${
+              isActive
+                ? "bg-primary text-white"
+                : "hover:text-primary hover:bg-primary-50"
+            }`
+          }
+          end
+        >
+          <div className="flex gap-4 items-center">
+            {<LayoutDashboard />}
+            Dashboard
+          </div>
+        </NavLink>
         {dashboardNavLinks.map((item) => (
           <NavLink
             key={item.href}
@@ -16,7 +33,6 @@ const Sidebar = () => {
                   : "hover:text-primary hover:bg-primary-50"
               }`
             }
-            end
           >
             <div className="flex gap-4 items-center">
               {<item.icon />}
--- src/constants/index.js
+++ src/constants/index.js
@@ -9,11 +9,11 @@ import {
 } from "lucide-react";
 
 export const dashboardNavLinks = [
-  { href: "/dashboard", label: "Dashboard", icon: LayoutDashboard },
+  // { href: "/dashboard", label: "Dashboard", icon: LayoutDashboard },
   { href: "/dashboard/manage-meals", label: "Manage Meals", icon: Utensils },
   { href: "/dashboard/deposits", label: "Deposits", icon: Wallet },
   { href: "/dashboard/expense", label: "Expense", icon: Receipt },
   { href: "/dashboard/bazar-list", label: "Bazar List", icon: ShoppingBasket },
   { href: "/dashboard/members", label: "Members", icon: Users },
-  { href: "/dashboard/profile", label: "My Profile", icon: User },
+  // { href: "/dashboard/profile", label: "My Profile", icon: User },
 ];
