e9e8f5e3bd76084e20d989047800da0c2e170e84
Mehedi Hasan <mehediyyzz@gmail.com>
2024-06-13 08:24:07 +0600

    blog card image fix

--- src/components/home/RecentBlog/BlogCard.jsx
+++ src/components/home/RecentBlog/BlogCard.jsx
@@ -6,7 +6,7 @@ const BlogCard = ({ data }) => {
   return (
     <div className="h-auto border hover:border-primary hover:shadow-md p-4 rounded-md space-y-3 duration-200 ease-in-out flex flex-col">
       <div className="overflow-hidden rounded h-[200px]">
-        <img className="h-full w-full bg-cover" src={data.img} alt="" />
+        <img className="h-full w-full object-cover" src={data.img} alt="" />
       </div>
       <div className="flex flex-col gap-2 flex-grow">
         <p className="text-grey-500 text-sm">{data.date}</p>
